adf829bc972725e78346f091a78b800f2c2d343d
Santiago Gonzalez <sgonzalezhernandez081@gmail.com>
2019-09-23 10:13:13 -0500

    consumir API con jquery y VanillaJS

--- src/js/home.js
+++ src/js/home.js
@@ -44,3 +44,23 @@ Promise.race([
 .catch((message)=>{
   console.log(message)
 })
+
+//jQuery
+// parametros url {configuracion}
+// $.ajax('https://randomuser.me/api/',{
+//   method: 'GET',
+//   //success todo bien
+//   success: function(data){
+//     console.log(data)
+//   },
+//   //error todo mal 
+//   error: function(error){
+//     console.log(error)
+//   }
+// })
+
+//vanillaJs
+fetch('https://randomuser.me/api/')
+  .then( (response)=> response.json() )
+  .then((data)=> console.log(data, data.results[0].name.first))
+  .catch(()=> console.log('algo fallo'))
